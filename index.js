5b75771a92dbefef22acb2882733ce362cdc6682
shrijanapoudel <shrijanapoudel@whitehatengineering.com>
2021-03-23 17:18:49 +0545

    change port and path

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const app = require("express")();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/:id", (req, res) => {
   const id = req.params.id;
@@ -7,4 +10,11 @@ app.get("/:id", (req, res) => {
   });
 });
 
-app.listen(3000);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
